f0c65e2ec479e27a5695b7ebdad8a9e3a9ac178a
michelebassi69 <117818525+michelebassi69@users.noreply.github.com>
2022-11-15 18:07:30 +0100

    Add files via upload

---
+++ HTML CREATOR.py
@@ -0,0 +1,25 @@
+#!python3
+
+
+import sys
+
+
+boold = True
+program_name = "htmlCreator.py"
+
+
+def main():
+    if boold:
+        print("Main")
+    # codice algoritmo
+
+
+if __name__ == "__main__":
+    if boold:
+        msg = "Esecuzione " + program_name + " con " + sys.version
+        print(msg)
+    
+    main()
+
+    if boold:
+        print("Termine esecuzione")
